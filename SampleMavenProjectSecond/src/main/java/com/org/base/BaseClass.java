e1be8bee9a32a43f7081653ff500ed2b4f29dea5
itssantoshyadav <itssantoshyadav@outlook.com>
2024-01-14 01:32:54 +0530

    3rd

--- SampleMavenProjectSecond/src/main/java/com/org/base/BaseClass.java
+++ SampleMavenProjectSecond/src/main/java/com/org/base/BaseClass.java
@@ -3,8 +3,12 @@ package com.org.base;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 
@@ -12,11 +16,20 @@ public class BaseClass {
 
 	@BeforeSuite
 	public void beforemethod() {
-		driver= new ChromeDriver();
-		driver.get("https://www.google.co.in/");
+		if (System.getProperty("browser").equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (System.getProperty("browser").equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (System.getProperty("browser").equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		driver.get(System.getProperty("url"));
 		driver.manage().window().maximize();
 		driver.findElement(By.xpath("//textarea[@aria-label='Search']")).sendKeys("Hello");
-		
+
 	}
 
 	@AfterSuite
