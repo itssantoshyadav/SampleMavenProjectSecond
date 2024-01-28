d6c67760ef53087a19fbd1d4e73164de40950b54
itssantoshyadav <itssantoshyadav@outlook.com>
2024-01-29 00:01:46 +0530

    3rd

---
+++ SampleMavenProjectSecond/src/test/java/com/org/test/SecondSample.java
@@ -0,0 +1,26 @@
+package com.org.test;
+
+import org.testng.annotations.Test;
+
+public class SecondSample {
+	@Test
+	public void methodOne() {
+		System.out.print("5");
+	}
+
+	@Test
+	public void methodTwo() {
+		System.out.print("6");
+	}
+
+	@Test
+	public void methodThree() {
+		System.out.print("7");
+	}
+
+	@Test
+	public void methodFour() {
+		System.out.print("8");
+	}
+
+}
